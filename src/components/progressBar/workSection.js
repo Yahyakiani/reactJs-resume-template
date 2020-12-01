60e26ca57d7a255efe49de9846c1b5762deaab4a
Yahya Kayani <yahyakiani1@gmail.com>
2020-12-01 22:05:02 +0500

    Additions in Work section

--- src/components/progressBar/workSection.js
+++ src/components/progressBar/workSection.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { PersonalDetails } from "../constants/data/ConstantData";
 import "./workSection.scss";
 
 const WorkSection = () => {
   return (
     <div className="work">
-          <h2>Work</h2>
-<h3><a href="">NMX Global Software</a></h3>
+      <h2>Work</h2>
+      <h3>
+        <a className="company" href={PersonalDetails.companyUrl}>
+          {PersonalDetails.companyName}
+        </a>
+      </h3>
+      <p class="info">
+        {PersonalDetails.position}
+        <span> • </span>
+        <span class="info-summary">{PersonalDetails.location}</span>
+        <span> • </span>
+        <em class="date">{PersonalDetails.workingTime}</em>
+      </p>
       <ul className="block-list">
-        <li>Foo</li>
+        {PersonalDetails.detail.map((detail) => (
+          <li>{detail.text}</li>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default WorkSection
+export default WorkSection;
